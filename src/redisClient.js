a3c35eb59dca5b71f8f1cf20ab53befed81628bb
Diego Pinheiro <126405977+DiegooPinheiro@users.noreply.github.com>
2026-04-08 14:55:13 -0300

    feat: initialize Redis client with environment configuration and add gitignore file

--- src/redisClient.js
+++ src/redisClient.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const redis = require('redis');
 const cliente = redis.createClient({
-    url: 'redis://default:0kUdMcYppetyVad0ydBVrqH8TH3JT4AW@redis-14257.crce278.sa-east-1-2.ec2.cloud.redislabs.com:14257'
+    url: process.env.REDIS_URL
 });
 
 cliente.on('connect', () => {
